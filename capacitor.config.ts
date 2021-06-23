3057ed5f41035d88927f4247c039156c3d2f0aa1
Jithin N Jose <jithinnjose@gmail.com>
2021-06-24 01:01:13 +0530

    CapacitorJS
    CapacitorJS Integration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.cowin.android',
+  appName: 'CowinVue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
